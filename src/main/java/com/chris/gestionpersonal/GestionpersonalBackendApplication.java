a1f3215de90dfeb690a41c42503b93f6aae6f9d6
Christian Hedz <cristian.28hedz@gmail.com>
2025-03-03 19:45:43 -0600

    init: commit inicial

---
+++ src/main/java/com/chris/gestionpersonal/GestionpersonalBackendApplication.java
@@ -0,0 +1,13 @@
+package com.chris.gestionpersonal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionpersonalBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionpersonalBackendApplication.class, args);
+    }
+
+}
