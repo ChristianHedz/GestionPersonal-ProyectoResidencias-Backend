cde42d71fee6d4393e304b59b57edd14ac7f6d21
Christian Hedz <cristian.28hedz@gmail.com>
2025-03-08 13:24:01 -0600

    feat: implementacion de la interfaz UserDetails en Employee, repositorios para Employee, Role,token y script de inicializacion de datos

--- src/main/java/com/chris/gestionpersonal/models/entity/Role.java
+++ src/main/java/com/chris/gestionpersonal/models/entity/Role.java
@@ -1,10 +1,9 @@
 package com.chris.gestionpersonal.models.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
+
+import java.util.Set;
 
 @Data
 @Entity
@@ -14,5 +13,7 @@ public class Role {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+    @OneToMany(mappedBy = "role", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private Set<Employee> employees;
 
 }
