05f969452c20fac2c70b3cb6d688044200778920
Christian Hedz <cristian.28hedz@gmail.com>
2025-03-08 14:39:45 -0600

    feat: implementacion del servicio de autenticacion, configuracion de seguridad HTTP y service para generacion de jwt

---
+++ src/main/java/com/chris/gestionpersonal/config/SecurityBeansInjector.java
@@ -0,0 +1,50 @@
+package com.chris.gestionpersonal.config;
+
+import com.chris.gestionpersonal.Repositories.EmployeeRepository;
+import com.chris.gestionpersonal.exceptions.ResourceNotFoundException;
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@AllArgsConstructor
+@Configuration
+public class SecurityBeansInjector {
+
+    private EmployeeRepository employeeRepository;
+
+    //AuthenticationManager es la interfaz principal para autenticar a un usuario.
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    //Crea un proveedor de autenticación el cual se conforma de un UserDetailsService y un PasswordEncoder
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setUserDetailsService(userDetailsService());
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+        return daoAuthenticationProvider;
+    }
+
+    //* PasswordEncoder: Interfaz que encripta y desencripta contraseñas.
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    //* UserDetailsService: Interfaz que carga los datos de un usuario.
+    @Bean
+    public UserDetailsService userDetailsService(){
+        return email -> employeeRepository.findByEmail(email)
+                .orElseThrow(() -> new ResourceNotFoundException("employee","email",email));
+    }
+
+}
---
+++ src/main/java/com/chris/gestionpersonal/services/AuthService.java
@@ -0,0 +1,66 @@
+package com.chris.gestionpersonal.services;
+
+import com.chris.gestionpersonal.Repositories.EmployeeRepository;
+import com.chris.gestionpersonal.Repositories.TokenRepository;
+import com.chris.gestionpersonal.exceptions.ResourceNotFoundException;
+import com.chris.gestionpersonal.mapper.EmployeeMapper;
+import com.chris.gestionpersonal.models.dto.AuthResponse;
+import com.chris.gestionpersonal.models.dto.EmployeeDTO;
+import com.chris.gestionpersonal.models.dto.LoginDTO;
+import com.chris.gestionpersonal.models.dto.RegisterDTO;
+import com.chris.gestionpersonal.models.entity.Employee;
+import com.chris.gestionpersonal.models.entity.Jwt;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+
+    private final EmployeeRepository employeeRepository;
+    private final EmployeeMapper employeeMapper;
+    private final EmployeeService employeeService;
+    private final JwtService jwtService;
+    private final AuthenticationManager authManager;
+    private final TokenRepository tokenRepository;
+    private static final String EMAIL = "email";
+
+    public AuthResponse login(LoginDTO loginDTO) {
+        Authentication auth =  new UsernamePasswordAuthenticationToken(
+                loginDTO.getEmail(),loginDTO.getPassword()
+        );
+        authManager.authenticate(auth);
+        Employee employee = employeeRepository.findByEmail(loginDTO.getEmail())
+                .orElseThrow(() -> new ResourceNotFoundException("Employee",EMAIL, loginDTO.getEmail()));
+        String jwt = jwtService.generateToken(employee,generateExtraClaims(employee));
+        saveToken(jwt,employee);
+        return new AuthResponse(jwt);
+    }
+
+    public EmployeeDTO register(RegisterDTO registerDTO) {
+        Employee employee = employeeService.register(registerDTO);
+        return employeeMapper.employeeToEmployeeDTO(employee);
+
+    }
+
+    private Map<String,Object> generateExtraClaims(Employee employee) {
+        return Map.of( EMAIL,employee.getEmail(),
+                  "authorities",employee.getAuthorities()
+        );
+    }
+
+    private void saveToken(String jwt,Employee employee) {
+        Jwt token = new Jwt();
+        token.setEmployee(employee);
+        token.setToken(jwt);
+        token.setExpiration(jwtService.extractExpiration(jwt));
+        token.setValid(true);
+        tokenRepository.save(token);
+    }
+
+}
