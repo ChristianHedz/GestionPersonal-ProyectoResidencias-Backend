4cbf552c01f3a600ddf8d6893b41679a2294f6d7
Christian Hedz <cristian.28@hedzgmail.com>
2025-03-08 19:35:39 -0600

    feat: Agregado filtro de autenticacion Jwt , Actualizado AuthResponse y registro de empleado para incluir jwt

---
+++ src/main/java/com/chris/gestionpersonal/config/JwtAuthenticationFilter.java
@@ -0,0 +1,69 @@
+package com.chris.gestionpersonal.config;
+
+import com.chris.gestionpersonal.Repositories.TokenRepository;
+import com.chris.gestionpersonal.exceptions.ResourceNotFoundException;
+import com.chris.gestionpersonal.models.entity.Employee;
+import com.chris.gestionpersonal.models.entity.Jwt;
+import com.chris.gestionpersonal.services.EmployeeService;
+import com.chris.gestionpersonal.services.JwtService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import java.io.IOException;
+import java.util.Date;
+import java.util.Optional;
+
+@AllArgsConstructor
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private JwtService jwtService;
+    private TokenRepository tokenRepository;
+    private EmployeeService employeeService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String jwt = jwtService.extractTokenFromRequest(request);
+        if(jwt == null || jwt.isBlank()){
+            filterChain.doFilter(request,response);
+            return;
+        }
+        Optional<Jwt> findToken = tokenRepository.findByToken(jwt);
+        boolean isValid = validateToken(findToken);
+        if(!isValid){
+            findToken.ifPresent(this::updateTokenStatus);
+            filterChain.doFilter(request,response);
+            return;
+        }
+        String email = jwtService.extractUsername(jwt);
+        Employee user = employeeService.findByEmail(email)
+                .orElseThrow(() -> new ResourceNotFoundException("employee","employee",email));
+        UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken
+                = new UsernamePasswordAuthenticationToken(email,null, user.getAuthorities());
+        usernamePasswordAuthenticationToken.setDetails(new WebAuthenticationDetails(request));
+        SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+        filterChain.doFilter(request,response);
+
+    }
+
+    private boolean validateToken(Optional<Jwt> optionalToken) {
+        if (optionalToken.isEmpty()){
+            return false;
+        }
+        Jwt token = optionalToken.get();
+        Date now = new Date((System.currentTimeMillis()));
+        return token.isValid() && token.getExpiration().after(now);
+    }
+
+    private void updateTokenStatus(Jwt token) {
+        token.setValid(false);
+        tokenRepository.save(token);
+    }
+}
