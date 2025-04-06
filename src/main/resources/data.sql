70839908ad83c746c6cec14e46bd505eb4290d57
Christian Hedz <cristian.28hedz@gmail.com>
2025-04-05 23:23:04 -0600

    feat: agregar registros de asistencia para el empleado admin en data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -10,3 +10,11 @@ VALUES ('admin', 'admin@gmail.com', '9876543210',
         '$2a$10$ywh1O2EwghHmFIMGeHgsx.9lMw5IXpg4jafeFS.Oi6nFv0181gHli',
         (SELECT id FROM role WHERE name = 'ADMIN'),
         (SELECT id FROM status WHERE name = 'ACTIVO'));
+
+INSERT INTO assist (date, entry_time, exit_time, incidents, worked_hours, employee_id)
+VALUES ('2025-04-04', '08:00:00', '17:00:00', 'FALTA', 9,
+        (SELECT id FROM employee WHERE full_name = 'admin')),
+       ('2025-04-04', '08:00:00', '17:00:00', 'RETARDO', 9,
+        (SELECT id FROM employee WHERE full_name = 'admin')),
+       ('2025-04-04', '08:00:00', '17:00:00', 'ASISTENCIA', 9,
+        (SELECT id FROM employee WHERE full_name = 'admin'));
