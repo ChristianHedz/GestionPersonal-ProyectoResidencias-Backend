1b10cd7a28334714496a857e7401c352a246d198
Christian Hedz <cristian.28hedz@gmail.com>
2025-04-18 00:22:04 -0600

    feat: agregue memoria para recordar conversaciones pasadas y refactorize configuración del cliente de chat

--- src/main/java/com/chris/gestionpersonal/config/ChatbotConfig.java
+++ src/main/java/com/chris/gestionpersonal/config/ChatbotConfig.java
@@ -2,6 +2,10 @@ package com.chris.gestionpersonal.config;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.AbstractChatMemoryAdvisor;
+import org.springframework.ai.chat.client.advisor.PromptChatMemoryAdvisor;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.chat.memory.InMemoryChatMemory;
 import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.mcp.SyncMcpToolCallbackProvider;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,13 +18,15 @@ public class ChatbotConfig {
 
     @Bean
     ChatClient chatClient(ChatModel chatModel, @Autowired(required = false) SyncMcpToolCallbackProvider toolCallbackProvider) {
+        ChatClient.Builder builder = ChatClient.builder(chatModel)
+                .defaultAdvisors(new SimpleLoggerAdvisor(),
+                        new PromptChatMemoryAdvisor(new InMemoryChatMemory()))
+                .defaultUser(user ->
+                        user.param(AbstractChatMemoryAdvisor.CHAT_MEMORY_RETRIEVE_SIZE_KEY, 3));
         if (toolCallbackProvider != null) {
-            return ChatClient
-                    .builder(chatModel)
-                    .defaultTools(toolCallbackProvider.getToolCallbacks())
-                    .build();
-        } else {
-            return ChatClient.builder(chatModel).build();
+            builder
+                .defaultTools(toolCallbackProvider.getToolCallbacks());
         }
+        return builder.build();
     }
 }
